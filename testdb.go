543db05aa6bdfd530a2c70c84bc1b15f22f898ad
Erik St. Martin <alakriti@gmail.com>
2012-09-26 15:17:54 -0400

    basic Query stubbing functionality, and ability to override driver.Open

--- testdb.go
+++ testdb.go
@@ -2,20 +2,65 @@ package testdb
 
 import (
 	"database/sql/driver"
+  "encoding/csv"
+  "strings"
+  "io"
+  "bytes"
+  "errors"
 )
 
+type opener func(dsn string) (driver.Conn, error)
+
 type Driver struct {
+  open opener
+  conn *Conn
 }
 
 func (d *Driver) Open(dsn string) (driver.Conn, error) {
-	return &Conn{}, nil
+  if d.open != nil {
+    conn, err := d.open(dsn)
+    return conn, err
+  }
+
+  if d.conn == nil {
+    d.conn = NewConn()
+  }
+
+  return d.conn, nil
+}
+
+func (d *Driver) SetOpen(f opener) {
+  d.open = f
+}
+
+func (d *Driver) SetConnection(conn *Conn){
+  d.conn = conn
 }
 
 type Conn struct {
+  queries map[string]Query
 }
 
-func (*Conn) Prepare(query string) (driver.Stmt, error) {
-	return &Stmt{}, nil
+func NewConn() (*Conn){
+  return &Conn{
+    queries: make(map[string]Query),
+  }
+}
+
+func (c *Conn) StubQuery(query string, result driver.Rows){
+  c.queries[query] = Query{
+    result: result,
+  }
+}
+
+func (c *Conn) Prepare(query string) (driver.Stmt, error) {
+  if q, ok := c.queries[query]; ok {
+    return &Stmt{
+      result: q.result,
+    }, nil
+  }
+
+  return &Stmt{}, errors.New("Query not stubbed: " + query)
 }
 
 func (*Conn) Close() error {
@@ -26,7 +71,12 @@ func (*Conn) Begin() (driver.Tx, error) {
 	return &Tx{}, nil
 }
 
+func (c *Conn) Exec(query string, args []driver.Value) (driver.Result, error) {
+  return nil, nil
+}
+
 type Stmt struct {
+  result driver.Rows
 }
 
 func (*Stmt) Close() error {
@@ -41,8 +91,8 @@ func (*Stmt) Exec(args []driver.Value) (driver.Result, error) {
 	return nil, nil
 }
 
-func (*Stmt) Query(args []driver.Value) (driver.Rows, error) {
-	return nil, nil
+func (s *Stmt) Query(args []driver.Value) (driver.Rows, error) {
+	return s.result, nil
 }
 
 type Tx struct {
@@ -54,4 +104,63 @@ func (*Tx) Commit() error {
 
 func (*Tx) Rollback() error {
 	return nil
+}
+
+type Rows struct {
+  closed bool
+  columns []string
+  rows [][]string
+  pos int
+}
+
+func (rs *Rows) Next(dest []driver.Value) error {
+	rs.pos++
+	if rs.pos >= len(rs.rows) {
+		return io.EOF // per interface spec
+	}
+
+  for i, col := range rs.rows[rs.pos-1] {
+    b := bytes.NewBufferString(col)
+
+    dest[i] = b.Bytes()
+  }
+
+  return nil
+}
+
+func (rs *Rows) Err() error {
+  return nil
+}
+
+func (rs *Rows) Columns() ([]string) {
+  return rs.columns
+}
+
+func (rs *Rows) Scan(dest ...interface{}) error {
+  return nil
+}
+
+func (rs *Rows) Close() error {
+  return nil
+}
+
+type Value struct {
+}
+
+type Query struct {
+  result driver.Rows
+}
+
+
+func RowsFromCSVString(columns []string, s string) (driver.Rows){
+  rows := &Rows{
+    columns: columns,
+  }
+
+  r := strings.NewReader(strings.TrimSpace(s))
+  csvReader := csv.NewReader(r)
+
+  rows.rows, _ = csvReader.ReadAll()
+
+  return rows
 }
--- testdb_test.go
+++ testdb_test.go
@@ -2,20 +2,85 @@ package testdb
 
 import (
 	"database/sql"
-	"fmt"
+  "database/sql/driver"
 	"testing"
+  "errors"
 )
 
-func TestQuery(t *testing.T) {
-	d := &Driver{}
-	sql.Register("testdb", d)
+var d *Driver
+var c *Conn
 
-	db, err := sql.Open("testdb", "")
+func init(){
+  if d == nil {
+    c = NewConn()
+    d = &Driver{}
+    sql.Register("testdb", d)
+  }
+}
+
+// Driver
+func TestSetOpen(t *testing.T) {
+  d.SetOpen(func (dsn string) (driver.Conn, error) {
+    return c, errors.New("test error")
+  })
+  defer d.SetOpen(nil)
+
+	db, _ := sql.Open("testdb", "foo")
+  conn, err := db.Driver().Open("foo")
+
+  if db == nil {
+    t.Fatal("driver.Open not properly set: db was nil")
+  }
+
+  if conn != c {
+    t.Fatal("driver.Open not properly set: db was not returned properly")
+  }
+
+  if err.Error() != "test error" {
+    t.Fatal("driver.Open not properly set: err was not returned properly")
+  }
+}
+
+func TestStubQuery(t *testing.T) {
+  conn := NewConn()
+
+  d.SetConnection(conn);
+  db, _ := sql.Open("testdb", "")
 
 	sql := "select count(*) from foo"
-	res, err := db.Query(sql)
+  columns := []string{"count"}
+  result := `
+  5
+  `
+  conn.StubQuery(sql, RowsFromCSVString(columns, result))
 
-	if err != nil {
-		fmt.Println(res)
-	}
+  res, err := db.Query(sql)
+
+  if res.Next() {
+    var count int64
+    err = res.Scan(&count)
+
+    if err != nil {
+      t.Fatal(err)
+    }
+
+    if count != 5 {
+      t.Fatal("failed to return count")
+    }
+  }
 }
+
+func TestUnknownQuery(t *testing.T){  conn := NewConn()
+  d.SetConnection(conn);
+  db, _ := sql.Open("testdb", "")
+
+	sql := "select count(*) from foobar"
+  _, err := db.Query(sql)
+
+  if err == nil {
+    t.Fatal("Unknown queries should fail")
+  }
+
+}
+
+
