dcf4133767e35daea6318fed5b61f2c25b67674d
Erik St. Martin <alakriti@gmail.com>
2013-02-09 17:30:10 -0500

    godoc love

--- testdb.go
+++ testdb.go
@@ -11,8 +11,6 @@ import (
 	"time"
 )
 
-type opener func(dsn string) (driver.Conn, error)
-
 var d *testDriver
 
 func init() {
@@ -21,7 +19,7 @@ func init() {
 }
 
 type testDriver struct {
-	open opener
+	open func(dsn string) (driver.Conn, error)
 	conn *conn
 }
 
@@ -61,36 +59,43 @@ func getQueryHash(query string) string {
 	return string(h.Sum(nil))
 }
 
+// Set your own function to be executed when db.Query() is called. As with StubQuery() you can use the RowsFromCSVString() method to easily generate the driver.Rows, or you can return your own.
 func SetQueryFunc(f func(query string) (result driver.Rows, err error)) {
 	d.conn.queryFunc = f
 }
 
+// Stubs the global driver.Conn to return the supplied driver.Rows when db.Query() is called, query stubbing is case insensitive, and whitespace is also ignored.
 func StubQuery(q string, result driver.Rows) {
 	d.conn.queries[getQueryHash(q)] = query{
 		result: result,
 	}
 }
 
+// Stubs the global driver.Conn to return the supplied error when db.Query() is called, query stubbing is case insensitive. and whitespace is also ignored.
 func StubQueryError(q string, err error) {
 	d.conn.queries[getQueryHash(q)] = query{
 		err: err,
 	}
 }
 
-func SetOpenFunc(f opener) {
+// Set your own function to be executed when db.Open() is called. You can either hand back a valid connection, or an error. Conn() can be used to grab the global Conn object containing stubbed queries.
+func SetOpenFunc(f func(dsn string) (driver.Conn, error)) {
 	d.open = f
 }
 
+// Clears all stubbed queries, and replaced functions.
 func Reset() {
 	d = newDriver()
 }
 
+// Returns a pointer to the global conn object associated with this driver.
 func Conn() driver.Conn {
 	return d.conn
 }
 
 var timeRegex, _ = regexp.Compile(`^\d{4}-\d{2}-\d{2}(\s\d{2}:\d{2}:\d{2})?$`)
 
+// Helper method to create a driver.Rows object to be used as part of stubbing a Query, the csv string can be any string supported by the csv package.
 func RowsFromCSVString(columns []string, s string) driver.Rows {
 	rs := &rows{
 		columns: columns,
